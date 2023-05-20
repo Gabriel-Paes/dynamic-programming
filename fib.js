5a72a70b57192ecff612b787c8880d1a3e47bd16
Gabriel-Paes <paessouzagabriel@gmail.com>
2023-05-20 08:38:24 -0300

    fib with memory obj

--- fib.js
+++ fib.js
@@ -1,9 +1,11 @@
-const fib = (n) => {
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
   if (n <= 2) return 1;
-  return fib(n - 1) + fib(n - 2);
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
 };
 
 console.log(fib(6)); // result -> 8
 console.log(fib(7)); // result -> 13
 console.log(fib(8)); // result -> 21
-//console.log(fib(50)); // result -> 12586269025
+console.log(fib(50)); // result -> 12586269025
